1fd8ec6841424778df44626d2843cbf36bef9b20
dclaiche <dclaiche@hotmail.com>
2022-03-02 12:52:21 -0800

    added dbcon.js

---
+++ dbcon.js
@@ -0,0 +1,9 @@
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'classmysql.engr.oregonstate.edu',
+  user            : 'cs340_claiched',
+  password        : '4471',
+  database        : 'cs340_claiched'
+});
+module.exports.pool = pool;
